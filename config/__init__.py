7d0cad32560528105d6262d6f53e30ca168389bf
Luke Tankey <luke.tankey@gmail.com>
2018-09-26 13:02:44 +1000

    Add Project-to-Milestone sync
    Tickets added to GitHub projects will be added to Milestones of the same name. Changes on a project
    board which is closed will not have an affect on the milestone.

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,12 @@
 # from os import environ
-from envparse import env
+from environ import Env as EnvironEnv
 from .kms import string_or_b64kms
 
 
-class Env(object):
-    def __call__(self, var, default=None, cast=None):
-        value = env(var, default=default, cast=cast)
+class Env(EnvironEnv):
+    def __call__(self, var, default=None, cast=None, parse_default=False):
+        value = self.get_value(
+            var, default=default, cast=cast, parse_default=parse_default)
         return string_or_b64kms(value)
 
 # class Env(object):
--- gnome/plugins/sync_project_milestones.py
+++ gnome/plugins/sync_project_milestones.py
@@ -1,6 +1,39 @@
 from gnome.policies import Policy
+from gnome.gh import Repo, Issue, gh
 
 
 class SyncProjectMilestones(Policy):
     def dispatch_gnome(self):
-        pass
+        headers = self.callback.headers()
+        event = headers['X-GitHub-Event']
+
+        if event not in ('project_card'):
+            return
+
+        payload = self.callback.payload()
+        card = payload['project_card']
+        if not payload['action'] == 'deleted':
+            if 'content_url' in card:
+                # Is a ticket
+                repo_name = payload['repository']['full_name']
+                issue_url = card['content_url']
+                issue_number = issue_url.split('/')[-1]
+                repo = Repo(repo_name)
+                gh_issue = repo._repo.get_issue(int(issue_number))
+                issue = Issue(repo, gh_issue)
+
+                # Project details
+                project_url = payload['project_card']['project_url']
+                project_id = project_url.split('/')[-1]
+                project = gh.get_project(int(project_id))
+                project_name = project.name
+
+                if project.state == 'closed':
+                    return
+
+                if gh_issue.milestone \
+                        and gh_issue.milestone.title == project_name:
+                    print('Milestone already set - aborting')
+                    return
+
+                issue.move_to_milestone(project_name)
